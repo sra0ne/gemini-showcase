67499435c591b3c45ee89d6203424937b18c04b3
sra0ne <sra0ne@skiff.com>
2024-02-07 21:16:36 +0530

    added error handling

--- gentext.rb
+++ gentext.rb
@@ -4,14 +4,15 @@ require 'net/http'
 require 'net/https'
 require 'json'
 require 'tty-markdown'
-def getprompt
-  puts 'Enter prompt'
+
+def get_prompt
+  puts 'Enter prompt:'
   gets.chomp
 end
 
-def getresponse(prompt)
-  puts 'Sending request'
-  uri = 	URI("https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=#{ENV['API_KEY']}")
+def get_response(prompt)
+  puts 'Sending request..'
+  uri = URI("https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=#{ENV['API_KEY']}")
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   request = Net::HTTP::Post.new(uri)
@@ -23,34 +24,51 @@ def getresponse(prompt)
       }]
     }]
   }.to_json
-  http.request(request)
+  response = http.request(request)
+  case response
+  when Net::HTTPSuccess
+    response
+  else
+    puts "HTTP Error: #{response.code} - #{response.message}"
+    rerun
+  end
 end
 
-def print(response)
+def print_output(response)
   raw = response.body
-  out = JSON.parse(raw)
-  out = out['candidates'][0]['content']['parts'][0]['text']
-  formatted_output = TTY::Markdown.parse(out)
-  puts formatted_output
+  parsed_response = JSON.parse(raw)
+  if parsed_response['promptFeedback'] && parsed_response['promptFeedback']['blockReason']
+    block_reason = parsed_response['promptFeedback']['blockReason']
+    prompt_feedback = parsed_response['promptFeedback']
+    prompt_feedback = JSON.pretty_generate(prompt_feedback)
+    puts "Response blocked due to: #{block_reason}"
+    puts "The whole prompt feedback is listed below:
+    #{prompt_feedback} " # verbose
+  else
+    out = parsed_response['candidates'][0]['content']['parts'][0]['text']
+    formatted_output = TTY::Markdown.parse(out)
+    puts formatted_output
+  end
 end
 
 def rerun
   loop do
-    puts 'Enter another Prompt(Y) or Exit(N)?'
+    puts 'Enter another prompt (Y) or Exit (N)?'
     choice = gets.chomp.upcase
     case choice
     when 'Y'
-      prompt = getprompt
-      response = getresponse(prompt)
-      print(response)
+      prompt = get_prompt
+      response = get_response(prompt)
+      print_output(response)
     when 'N'
       exit
     else
-      puts 'Invalid'
+      puts 'Invalid input,try again.'
     end
   end
 end
-prompt = getprompt
-response = getresponse(prompt)
-print(response)
+
+prompt = get_prompt
+response = get_response(prompt)
+print_output(response)
 rerun
